cc002c66f46d37fc35af3378085860729ab9dcbc
TotoAJ <matiasjuniel08@gmail.com>
2026-03-03 12:40:21 -0800

    modified:   src/client/java/totoaj/totosmacromod/event/KeyInputHandler.java

--- src/client/java/totoaj/totosmacromod/event/KeyInputHandler.java
+++ src/client/java/totoaj/totosmacromod/event/KeyInputHandler.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import totoaj.totosmacromod.TotosMacroMod;
@@ -16,8 +17,8 @@ public class KeyInputHandler {
             .register(ResourceLocation.fromNamespaceAndPath(TotosMacroMod.MOD_ID,
                     "macros"));
     private static final String KEY_TOGGLE_MACE = "key." + TotosMacroMod.MOD_ID + ".toggle_mace";
-    private static final String KEY_BREACH_MACE = "key." + TotosMacroMod.MOD_ID + ".breach_mace";
     private static final String KEY_DENSITY_MACE = "key." + TotosMacroMod.MOD_ID + ".density_mace";
+    private static final String KEY_BREACH_MACE = "key." + TotosMacroMod.MOD_ID + ".breach_mace";
 
     private static KeyMapping maceToggleKey;
     private static KeyMapping breachMaceKey;
@@ -48,10 +49,11 @@ public class KeyInputHandler {
 
                     previousSlot = client.player.getInventory().getSelectedSlot();
 
-                    int breachSlot = Integer.parseInt(
-                            breachMaceKey.saveString().split("\\.")[2]) - 1;
-                    int densitySlot = Integer.parseInt(
-                            densityMaceKey.saveString().split("\\.")[2]) - 1;
+                    Component breachKey = breachMaceKey.getTranslatedKeyMessage();
+                    Component densityKey = densityMaceKey.getTranslatedKeyMessage();
+
+                    int breachSlot = Integer.parseInt(breachKey.getString()) - 1;
+                    int densitySlot = Integer.parseInt(densityKey.getString()) - 1;
 
                     if (client.player.fallDistance < 6.5) {
                         if (breachSlot >= 0 && breachSlot <= 8) {
@@ -90,16 +92,16 @@ public class KeyInputHandler {
                 GLFW.GLFW_KEY_BACKSLASH,
                 MACRO_CATEGORY));
 
-        breachMaceKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                KEY_BREACH_MACE,
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_4,
-                MACRO_CATEGORY));
-
         densityMaceKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 KEY_DENSITY_MACE,
                 InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_3,
+                MACRO_CATEGORY));
+
+        breachMaceKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                KEY_BREACH_MACE,
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_4,
                 MACRO_CATEGORY));
     }
 }
